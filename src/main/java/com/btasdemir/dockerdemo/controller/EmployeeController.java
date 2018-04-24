690e2db74ea73f64d7a74ec3f0c166eab980dd7c
bahadir.tasdemir@trendyol.com <bahadir.tasdemir@trendyol.com>
2018-04-24 17:26:05 +0300

    small fixes

--- src/main/java/com/btasdemir/dockerdemo/controller/EmployeeController.java
+++ src/main/java/com/btasdemir/dockerdemo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.btasdemir.dockerdemo.controller;
 
 import com.btasdemir.dockerdemo.domain.dto.EmployeeDto;
 import com.btasdemir.dockerdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
